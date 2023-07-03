16bdaa86f47c1e68e3e84889f78a7abf3fc94198
PULA KARTHIK <125720219+Mrkk01@users.noreply.github.com>
2023-07-03 10:44:38 +0530

    Add files via upload

---
+++ Lecture14-CipherSchools.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a[10];
+	cout<<"values for comparion : "<<endl;
+	cout<<"the value of address a[0] is "<<&a[0]<<endl;
+	cout<<"the value of address a[1] is "<<&a[1];
+}
